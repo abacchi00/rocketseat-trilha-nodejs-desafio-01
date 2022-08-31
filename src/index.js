d48eb5a4c567b217358bf73827086c059eb6bedd
abacchi00 <andre.rossini.bacchi@gmail.com>
2022-08-30 21:07:48 -0300

    feat: PUT, PATCH, DELETE todo + middleware

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ app.use(express.json());
 const users = [];
 
 const findUser = (username) => users.find(user => user.username === username);
+const findTodo = (user, todoId) => user.todos.find(todo => todo.id === todoId);
 
 function checksExistsUserAccount(request, response, next) {
   const { username } = request.headers;
@@ -20,6 +21,20 @@ function checksExistsUserAccount(request, response, next) {
   if (!user) return response.status(400).json({ error: 'User not found for request header\'s username' });
 
   request.user = user;
+
+  return next();
+}
+
+// Always should be usead after checksExistsUserAccount middleware
+function checksExistsUserTodo(request, response, next) {
+  const { user } = request;
+  const { id: todoId } = request.params;
+
+  const todo = findTodo(user, todoId);
+
+  if (!todo) return response.status(404).json({ error: 'Todo not found' });
+
+  request.todo = todo;
 
   return next();
 }
@@ -48,7 +63,7 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 
   const newTodo = {
     title,
-    deadline: new Date(deadline + '00:00'),
+    deadline: new Date(deadline),
     id: uuidv4(),
     created_at: new Date(),
     done: false,
@@ -59,16 +74,30 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
   return response.status(201).json(newTodo);
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.put('/todos/:id', checksExistsUserAccount, checksExistsUserTodo, (request, response) => {
+  const { title, deadline } = request.body;
+  const { todo } = request;
+
+  todo.title = title;
+  todo.deadline = deadline;
+
+  return response.status(201).json(todo);
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.patch('/todos/:id/done', checksExistsUserAccount, checksExistsUserTodo, (request, response) => {
+  const { todo } = request;
+
+  todo.done = true;
+
+  return response.status(201).json(todo);
 });
 
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.delete('/todos/:id', checksExistsUserAccount, checksExistsUserTodo, (request, response) => {
+  const { user, todo } = request;
+
+  user.todos.splice(todo, 1);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
